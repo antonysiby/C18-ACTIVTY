6b94f8fc1c270a5a9ea9a342580896be2e904a43
antonysiby <91169646+antonysiby@users.noreply.github.com>
2021-11-16 10:41:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var s1
+function setup() {
+  createCanvas(400,400);
+s1 = new Student("paul",12,6);  
+
+s1.display(s1)
+var name = "john"
+var num = 31
+arr = ["item1",32,name,num]
+console.log(arr[0])
+console.log(arr);
+
+}
+
+function draw() 
+{
+  background(30);
+  this.show(Student)
+  
+}
